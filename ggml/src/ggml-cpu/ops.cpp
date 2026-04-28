d7ecc59a81af127a5be5d9c5c5500fbd1109ed7d
Geoff Munn <geoff@zimoobo.com>
2026-04-28 22:25:00 +1200

    Q4 build errors fixed

--- ggml/src/ggml-cpu/ops.cpp
+++ ggml/src/ggml-cpu/ops.cpp
@@ -4859,6 +4859,19 @@ void ggml_compute_forward_get_rows(
         case GGML_TYPE_IQ4_XS:
         case GGML_TYPE_IQ3_S:
         case GGML_TYPE_IQ2_S:
+        case GGML_TYPE_Q3_K_HIFI:
+        case GGML_TYPE_Q6_K_HIFI:
+        case GGML_TYPE_Q6_K_HIFI_DYNAMIC:
+        case GGML_TYPE_Q6_K_HIFI_RES8:
+        case GGML_TYPE_Q5_K_HIFI_RES8:
+        case GGML_TYPE_Q3_K_HIFI_RES8:
+        case GGML_TYPE_Q4_K_HIFI:
+        case GGML_TYPE_Q2_K_HIFI:
+        case GGML_TYPE_Q2_K_LITE:
+        case GGML_TYPE_Q3_K_LITE:
+        case GGML_TYPE_Q4_K_LITE:
+        case GGML_TYPE_Q5_K_LITE:
+        case GGML_TYPE_Q6_K_LITE:
             {
                 ggml_compute_forward_get_rows_q(params, dst);
             } break;
